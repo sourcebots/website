39feaec9a3a0e504f0245053b02db65783765921
Jake Howard <git@theorangeone.net>
2017-10-07 17:05:34 +0100

    Add main banner

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,7 @@ mkdir -p static/build/js static/build/css
 
 hugo gen chromastyles --style=monokai > static/src/scss/highlight.css
 cp -r node_modules/font-awesome/fonts static/build/fonts
+cp -r static/src/img static/build
 node-sass static/src/scss/index.scss static/build/css/index.css --source-map-embed
 browserify static/src/js/index.js -o static/build/js/app.js
 
