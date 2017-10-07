695dd6eec64adf602a8f78940d213ec8b7d4a2aa
Jake Howard <git@theorangeone.net>
2017-10-07 13:17:14 +0100

    Setup base static files

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ rm -rf static/build
 mkdir -p static/build/js static/build/css
 
 hugo gen chromastyles --style=monokai > static/src/scss/highlight.css
-
+cp -r node_modules/font-awesome/fonts static/build/fonts
 node-sass static/src/scss/index.scss static/build/css/index.css --source-map-embed
 browserify static/src/js/index.js -o static/build/js/app.js
 
---
+++ static/src/js/jquery-global.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var jQuery = require('jquery');
+
+window.$ = jQuery;
+window.jQuery = jQuery;
+global.$ = jQuery;
+global.jQuery = jQuery;
+
+module.exports = jQuery;
