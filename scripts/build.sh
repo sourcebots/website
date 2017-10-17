b0af1918e28f3f28347f5e37b40a3bb2682d3012
Jake Howard <git@theorangeone.net>
2017-10-17 21:33:34 +0100

    Add png of logo

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,5 @@ rm -rf public/
 
 hugo -v
 
-cp public/img/logo.svg public/favicon.svg
-rm public/favicon.ico
+cp public/img/logo.png public/favicon.ico
 sed -i -e 's/#e64946/#4B35B3/g' public/css/style.css
