2d413e311f28ad5ca5ef485e628837b0f2a6b4e0
Jake Howard <git@theorangeone.net>
2017-10-17 21:23:47 +0100

    Update deployer script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ "${CIRCLE_BRANCH}" != "master" ]; then
+    echo "Only master branch can be deployed"
+    exit 0
+fi
+
 set -e
 
 DEPLOY_DIR=deploy
@@ -11,8 +16,8 @@ git config --global user.name $CIRCLE_USERNAME
 git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
 cd $DEPLOY_DIR
-rsync -arv --delete ../public/* .
-
+rm -rf *
+cp ../public/* . -r
 git add -f .
 git commit -m "Deploy build $CIRCLE_BUILD_NUM [ci skip]" || true
 git push -f
